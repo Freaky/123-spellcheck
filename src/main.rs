56898f2e1c576ed65eb2af9b50942ca280cac5a5
Thomas Hurst <tom@hur.st>
2018-09-04 17:26:56 +0100

    Initial commit of prototype.

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+
+extern crate mailparse;
+extern crate select;
+extern crate ispell;
+
+use mailparse::*;
+
+use select::document::Document;
+use select::predicate::Name;
+
+use ispell::SpellLauncher;
+
+use std::io::{self, Read};
+
+const LANG: &str = "en_GB";
+
+fn main() {
+    let mut input = Vec::new();
+    io::stdin().read_to_end(&mut input).expect("reading input");
+
+    let mail = parse_mail(&input).expect("parsing email");
+
+    let mut speller = SpellLauncher::new()
+                          .hunspell()
+                          .dictionary(LANG)
+                          .launch()
+                          .expect("Can't run spell checker");
+
+    // fall back to raw input?
+    let body = mail.get_body().expect("Can't extract email body");
+
+    let doc = Document::from(&body[..]);
+
+    println!(
+"<html>
+  <head>
+    <meta charset='UTF-8'>
+    <style>
+      mark {{ background-color: purple;color: white; }}
+    </style>
+  </head>
+  <body>
+");
+    for row in doc.find(Name("tr")) {
+        let mut cols = row.find(Name("td"));
+
+        let question = cols.next().expect("Couldn't find question").text();
+        let answer = cols.next().expect("Couldn't find answer").text();
+
+        // squish multiple blank lines.
+        let answer = answer.lines()
+                           .filter(|s| { !s.is_empty() })
+                           .collect::<Vec<&str>>()
+                           .join("\n\n");
+
+        let corrected = answer.lines()
+            .map(|line| {
+                // Work word-by-word to make marking them easier.  Sadly loses things
+                // like double-spaces.
+                line.split_whitespace().map(|word|
+                    {
+                        let errors = speller.check(word).expect("Spellcheck error");
+
+                        if errors.is_empty() {
+                            word.to_string()
+                        } else {
+                            format!("<mark>{}</mark>", word)
+                        }
+                    }
+                ).collect::<Vec<String>>().join(" ")
+            }).collect::<Vec<String>>().join("<br>\n");
+
+        println!("<section>\n<h1>{}</h1>\n<p>{}</p></section>", question, corrected);
+    }
+    println!("</body></html>");
+}
